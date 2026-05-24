101b129266338c9942e0be1595d1ba5e481a1f7e
sharmashruchita28-shr <sharmashruchita28@gmail.com>
2026-05-24 16:16:43 +0530

    01-folder

---
+++ app.js
@@ -0,0 +1,7 @@
+// const a = 10
+// export default a
+
+const arr = [10, 20, 30, 40, 50]
+const user = 'Shruchita'
+export default user
+
---
+++ script.jsx
@@ -0,0 +1,11 @@
+// var h1 = document.createElement('h1')
+// h1.innerHTML = 'Hello From JS'
+// console.log(h1)
+// document.body.appendChild(h1)
+
+
+// import a from './app.js'
+// console.log(a)
+
+import user from './app.js'
+console.log(user)
